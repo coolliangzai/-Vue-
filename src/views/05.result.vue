3170b93c298133670c15e08c6bceed85aa62df65
谢炳吉 <1053516431@qq.com>
2020-08-19 13:26:51 +0800

    基本功能已经完成后面继续完善8/19

--- src/views/05.result.vue
+++ src/views/05.result.vue
@@ -2,6 +2,7 @@
   <div class="result-container">
     <div class="title-wrap">
       <h2 class="title">{{$route.query.q}}</h2>
+      <!-- 搜索结果总数 -->
       <span class="sub-title">{{count}}</span>
     </div>
     <el-tabs v-model="activeIndex">
@@ -41,138 +42,44 @@
       </el-tab-pane>
       <el-tab-pane label="歌单" name="lists">
         <div class="items">
-          <div class="item">
+          <div
+            class="item"
+            v-for="(item,index) in playList"
+            :key="index"
+            @click="toPlayList(item.id)"
+          >
             <div class="img-wrap">
               <div class="num-wrap">
                 播放量:
-                <span class="num">66892</span>
+                <span class="num">{{item.playCount}}</span>
               </div>
-              <img src="../assets/cover.jpg" alt />
+              <img :src="item.coverImgUrl" alt />
               <span class="iconfont icon-play"></span>
             </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
+            <p class="name">{{item.name}}</p>
           </div>
         </div>
       </el-tab-pane>
       <el-tab-pane label="MV" name="mv">
         <div class="items mv">
-          <div class="item">
+          <div
+            class="item"
+            v-for="(item,index) in mvList"
+            :key="index"
+            @click="toPlayMvList(item.id)"
+          >
             <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
+              <img :src="item.cover" alt />
               <span class="iconfont icon-play"></span>
               <div class="num-wrap">
                 <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
+                <div class="num">{{item.playCount}}</div>
               </div>
-              <span class="time">02:43</span>
+              <span class="time">{{item.duration}}</span>
             </div>
             <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-              <div class="num-wrap">
-                <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
-              </div>
-              <span class="time">02:43</span>
-            </div>
-            <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-              <div class="num-wrap">
-                <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
-              </div>
-              <span class="time">02:43</span>
-            </div>
-            <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-              <div class="num-wrap">
-                <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
-              </div>
-              <span class="time">02:43</span>
-            </div>
-            <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt />
-              <span class="iconfont icon-play"></span>
-              <div class="num-wrap">
-                <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
-              </div>
-              <span class="time">02:43</span>
-            </div>
-            <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
+              <div class="name">{{item.name}}</div>
+              <div class="singer">{{item.artistName}}</div>
             </div>
           </div>
         </div>
@@ -192,7 +99,116 @@ export default {
       // 搜索结果的总数
       count: 0,
       musicUrl: "",
+      type: 1,
+      playList: [],
+      mvList: [],
     };
+  },
+  watch: {
+    activeIndex() {
+      switch (this.activeIndex) {
+        case "songs":
+          this.type = 1;
+          // console.log(this.type);
+          break;
+        case "lists":
+          this.type = 1000;
+          //  console.log(this.type);
+          break;
+        case "mv":
+          this.type = 1004;
+          //  console.log(this.type);
+          break;
+
+        default:
+          break;
+      }
+      if (this.type == 1) {
+        //获取搜索结果
+        axios({
+          method: "get",
+          url: "https://autumnfish.cn/search",
+          params: {
+            keywords: this.$route.query.q,
+            //单曲
+            type: this.type,
+            //获取的数据量
+            limit: 10,
+          },
+        }).then((res) => {
+          this.musicList = res.data.result.songs;
+          // console.log(this.musicList);
+          this.count = res.data.result.songCount;
+          this.musicList.forEach(function (val) {
+            let time = val.duration;
+            let sec = parseInt((val.duration / 1000) % 60);
+            if (sec < 10) {
+              sec = "0" + sec;
+            }
+            let min = parseInt(val.duration / 1000 / 60);
+            if (min < 10) {
+              min = "0" + min;
+            }
+            val.duration = min + ":" + sec;
+          });
+        });
+      } else if (this.type == 1000) {
+        //获取搜索结果
+        axios({
+          method: "get",
+          url: "https://autumnfish.cn/search",
+          params: {
+            keywords: this.$route.query.q,
+            //单曲
+            type: this.type,
+            //获取的数据量
+            limit: 10,
+          },
+        }).then((res) => {
+          this.count = res.data.result.playlistCount;
+          this.playList = res.data.result.playlists;
+          //console.log(res.data.result.playlists);
+          this.playList.forEach(function (val) {
+            if (val.playCount > 10000) {
+              val.playCount = parseInt(val.playCount / 100000) + "万";
+            }
+          });
+        });
+      } else if (this.type == 1004) {
+        //获取搜索结果
+        axios({
+          method: "get",
+          url: "https://autumnfish.cn/search",
+          params: {
+            keywords: this.$route.query.q,
+            //单曲
+            type: this.type,
+            //获取的数据量
+            limit: 12,
+          },
+        }).then((res) => {
+          this.count = res.data.result.mvCount;
+          // console.log(res.data.result.mvCount);
+          this.mvList = res.data.result.mvs;
+          console.log(res);
+          this.mvList.forEach(function (val) {
+            if (val.playCount > 10000) {
+              val.playCount = parseInt(val.playCount / 10000) + "万";
+              let time = val.duration;
+              let sec = parseInt((val.duration / 1000) % 60);
+              if (sec < 10) {
+                sec = "0" + sec;
+              }
+              let min = parseInt(val.duration / 1000 / 60);
+              if (min < 10) {
+                min = "0" + min;
+              }
+              val.duration = min + ":" + sec;
+            }
+          });
+        });
+      }
+    },
   },
   methods: {
     // 抽取出播放音乐的方法
@@ -208,11 +224,17 @@ export default {
         this.musicUrl = res.data.data[0].url;
       });
     },
+    toPlayList(id) {
+      this.$router.push("./playlist?q=" + id);
+    },
     //双击播放音乐的功能
     playMusic(id) {
       this.playAxios(id);
       // 将当前组件的musicUrl传给父组件的musicUrl
       this.$parent.musicUrl = this.musicUrl;
+    },
+    toPlayMvList(id) {
+      this.$router.push("./mv?q=" + id);
     },
   },
   created() {
--- src/views/07.mv.vue
+++ src/views/07.mv.vue
@@ -4,55 +4,44 @@
       <h3 class="title">mv详情</h3>
       <!-- mv -->
       <div class="video-wrap">
-        <video
-          controls
-          src="https://nie.v.netease.com/r/video/20180531/44f868de-deef-4409-8325-3bb3b5567f2c.mp4"
-        ></video>
+        <video controls :src="mvurl"></video>
       </div>
       <!-- mv信息 -->
       <div class="info-wrap">
         <div class="singer-info">
           <div class="avatar-wrap">
-            <img src="../assets/avatar.jpg" alt="" />
+            <img :src="mvdetails.cover" alt />
           </div>
-          <span class="name">TF Boys</span>
+          <span class="name">{{mvdetails.artistName}}</span>
         </div>
         <div class="mv-info">
-          <h2 class="title">TF BOYS LIVE 秀 王源《淘汰》</h2>
-          <span class="date">发布：2014-11-04</span>
-          <span class="number">播放：94526次</span>
-          <p class="desc">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consequuntur saepe aut officia itaque exercitationem culpa facere
-            doloremque voluptates id non nam, aliquid ipsum laborum odit
-            accusantium dolorem eligendi veniam dolore ea aperiam labore
-            cupiditate et a. Necessitatibus eaque blanditiis possimus nobis
-            ullam reprehenderit animi, vero reiciendis eos, deleniti commodi,
-            consequatur dolorem iusto. Assumenda doloribus soluta temporibus ut
-            dolorum corporis quos! Quisquam consectetur dolore iste quo
-            praesentium dolorum excepturi, at sapiente pariatur quis! Neque ex
-            cum, nobis aspernatur temporibus, voluptates at obcaecati dolore est
-            repudiandae, veniam laborum fuga corrupti illum ut. Ad a tempore
-            sint adipisci vero, delectus ducimus debitis molestias!
-          </p>
+          <h2 class="title">{{mvdetails.briefDesc}}</h2>
+          <span class="date">发布：{{mvdetails.publishTime}}</span>
+          <span class="number">播放：{{mvdetails.playCount}}次</span>
+          <p class="desc">{{mvdetails.desc}}</p>
         </div>
       </div>
       <!-- 精彩评论 -->
       <div class="comment-wrap">
-        <p class="title">精彩评论<span class="number">(666)</span></p>
+        <p class="title">
+          精彩评论
+          <span class="number">({{hotComments.length}})</span>
+        </p>
         <div class="comments-wrap">
-          <div class="item">
+          <div class="item" v-for="(item,index) in hotComments" :key="index">
             <div class="icon-wrap">
-              <img src="../assets/avatar.jpg" alt="" />
+              <!-- 头像 -->
+              <img :src="item.user.avatarUrl" alt />
             </div>
             <div class="content-wrap">
               <div class="content">
-                <span class="name">爱斯基摩：</span>
-                <span class="comment">谁说的，长大了依旧可爱哈</span>
+                <!-- 昵称 -->
+                <span class="name">{{item.user.nickname}}：</span>
+                <span class="comment">{{item.content}}</span>
               </div>
-              <div class="re-content">
-                <span class="name">小苹果：</span>
-                <span class="comment">还是小时候比较可爱</span>
+              <div class="re-content" v-if="item.beReplied.length!=0">
+                <span class="name">{{item.beReplied[0].user.nickname}}：</span>
+                <span class="comment">{{item.beReplied[0].content}}</span>
               </div>
               <div class="date">2020-02-12 17:26:11</div>
             </div>
@@ -61,52 +50,23 @@
       </div>
       <!-- 最新评论 -->
       <div class="comment-wrap">
-        <p class="title">最新评论<span class="number">(666)</span></p>
+        <p class="title">
+          最新评论
+          <span class="number">({{totalNum}})</span>
+        </p>
         <div class="comments-wrap">
-          <div class="item">
+          <div class="item" v-for="(item,index) in comments" :key="index">
             <div class="icon-wrap">
-              <img src="../assets/avatar.jpg" alt="" />
+              <img :src="item.user.avatarUrl" alt />
             </div>
             <div class="content-wrap">
               <div class="content">
-                <span class="name">爱斯基摩：</span>
-                <span class="comment">谁说的，长大了依旧可爱哈</span>
+                <span class="name">{{item.user.nickname}}：</span>
+                <span class="comment">{{item.content}}</span>
               </div>
-              <div class="re-content">
-                <span class="name">小苹果：</span>
-                <span class="comment">还是小时候比较可爱</span>
-              </div>
-              <div class="date">2020-02-12 17:26:11</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="icon-wrap">
-              <img src="../assets/avatar.jpg" alt="" />
-            </div>
-            <div class="content-wrap">
-              <div class="content">
-                <span class="name">爱斯基摩：</span>
-                <span class="comment">谁说的，长大了依旧可爱哈</span>
-              </div>
-              <div class="re-content">
-                <span class="name">小苹果：</span>
-                <span class="comment">还是小时候比较可爱</span>
-              </div>
-              <div class="date">2020-02-12 17:26:11</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="icon-wrap">
-              <img src="../assets/avatar.jpg" alt="" />
-            </div>
-            <div class="content-wrap">
-              <div class="content">
-                <span class="name">爱斯基摩：</span>
-                <span class="comment">谁说的，长大了依旧可爱哈</span>
-              </div>
-              <div class="re-content">
-                <span class="name">小苹果：</span>
-                <span class="comment">还是小时候比较可爱</span>
+              <div class="re-content" v-if="item.beReplied.length!=0">
+                <span class="name">{{item.beReplied[0].user.nickname}}：</span>
+                <span class="comment">{{item.beReplied[0].content}}</span>
               </div>
               <div class="date">2020-02-12 17:26:11</div>
             </div>
@@ -122,26 +82,25 @@
         :current-page="page"
         :page-size="5"
         :limit="limit"
-      >
-      </el-pagination>
+      ></el-pagination>
     </div>
     <div class="mv-recommend">
       <h3 class="title">相关推荐</h3>
       <div class="mvs">
         <div class="items">
-          <div class="item">
+          <div class="item" v-for="(item,index) in recommendMvList" :key="index">
             <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt="" />
+              <img :src="item.cover" alt />
               <span class="iconfont icon-play"></span>
               <div class="num-wrap">
                 <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
+                <div class="num">{{item.playCount}}</div>
               </div>
-              <span class="time">02:43</span>
+              <span class="time">{{item.duration}}</span>
             </div>
             <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
+              <div class="name">{{item.name}}</div>
+              <div class="singer">{{item.artistName}}</div>
             </div>
           </div>
         </div>
@@ -151,23 +110,126 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'mv',
+  name: "mv",
   data() {
     return {
       // 总条数
-      total: 20,
+      total: 0,
+      totalNum: 0,
       // 页码
       page: 1,
       // 页容量
-      limit: 10
+      limit: 10,
+      //存储mv播放链接
+      mvurl: "",
+      mvid: 0,
+      //推荐mv列表
+      recommendMvList: [],
+      mvdetails: "",
+      //热门评论
+      hotComments: [],
+      //最新评论
+      comments: [],
+      limit: 20,
     };
+  },
+  created() {
+    //获取评论信息
+    axios({
+      method: "get",
+      url: "https://autumnfish.cn/comment/mv",
+      params: {
+        id: this.$route.query.q,
+        limit: this.limit,
+        offset: (this.page - 1) * 5,
+      },
+    }).then((res) => {
+      this.comments = res.data.comments;
+      this.hotComments = res.data.hotComments;
+      this.totalNum = res.data.total;
+      this.total = res.data.total;
+      console.log(res.data);
+    });
+    //获取mv播放的链接
+    axios({
+      method: "get",
+      url: "https://autumnfish.cn/mv/url",
+      params: {
+        id: this.$route.query.q,
+      },
+    }).then((res) => {
+      this.mvurl = res.data.data.url;
+      this.mvid = res.data.data.id;
+      // console.log(this.mvid);
+      //  console.log(res);
+    });
+    //获取相关推荐的mv
+    axios({
+      mehtod: "get",
+      url: "https://autumnfish.cn/simi/mv",
+      params: {
+        mvid: this.$route.query.q,
+      },
+    }).then((res) => {
+      this.recommendMvList = res.data.mvs;
+      //对播放数量进行处理，当大于一万时，用万为单位
+      this.recommendMvList.forEach(function (val) {
+        if (val.playCount > 10000) {
+          val.playCount = parseInt(val.playCount / 10000) + "万";
+        }
+        // 把mv的时长进行格式化
+        let min = parseInt(val.duration / 1000 / 60);
+        if (min < 10) {
+          min = "0" + min;
+        }
+        let sec = (val.duration / 1000) % 60;
+        if (sec < 10) {
+          sec = "0" + sec;
+        }
+        val.duration = min + ":" + sec;
+      });
+      // console.log(res.data.mvs);
+    });
+    //获取mv的信息
+    axios({
+      method: "get",
+      url: "https://autumnfish.cn/mv/detail",
+      params: {
+        mvid: this.$route.query.q,
+      },
+    }).then((res) => {
+      this.mvdetails = res.data.data;
+      if (res.data.data.playCount > 10000) {
+        res.data.data.playCount =
+          parseInt(res.data.data.playCount / 10000) + "万";
+      }
+      console.log(this.mvdetails);
+      // console.log(res);
+    });
   },
   methods: {
     handleCurrentChange(val) {
+      this.page = val;
+      //获取评论信息
+      axios({
+        method: "get",
+        url: "https://autumnfish.cn/comment/mv",
+        params: {
+          id: this.$route.query.q,
+          limit: this.limit,
+          offset: (this.page - 1) * 5,
+        },
+      }).then((res) => {
+        this.comments = res.data.comments;
+        // this.hotComments = res.data.hotComments;
+        this.totalNum = res.data.total;
+        console.log(res.data);
+      });
       console.log(`当前页: ${val}`);
-    }
-  }
+    },
+  },
 };
 </script>
 
